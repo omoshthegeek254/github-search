88d909883555f83af8cae15948ffdf958eed935c
omoshthegeek <omoshthegeek@gmail.com>
2019-12-09 00:05:10 +0300

    added general layou of profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tut3';
+  title = 'Github Search';
 }
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,7 +1,7 @@
 export class Repository {
-constructor(public name:string,
-    public html_url:string,
-    public description:string,
-    public created_at:Date) {}
+public name:string;
+    public html_url:string;
+    public description:string;
+    public created_at:Date;
 }
-}
+
